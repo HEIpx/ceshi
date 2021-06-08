cec6f14e482a4903a34f2220859162a070e3a3d2
HEI <2878060043@qq.com>
2021-06-08 13:30:28 +0800

    ..

--- input/src/views/About.vue
+++ input/src/views/About.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="about">
-  
+  <One />
+  <Two />
   </div>
 </template>
 <script>
+import One from '../components/zujian/One'
+import Two from '../components/zujian/Two'
 export default {
+  components: { One,Two },
   data(){
     return {
       msg:{ }
